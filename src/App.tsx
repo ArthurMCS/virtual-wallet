0f620b8c96eb50327257d68c7417ce27a7f8f5bb
Arthur Moreira <arthurmcs96@gmail.com>
2022-09-15 23:49:21 -0300

    cria o form dentro do modal e css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import Dashboard from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from "react-modal";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { useState } from "react";
+
+Modal.setAppElement('#root')
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModal] = useState(false)
+
+  const handleOpenModal = () => setIsNewTransactionModal(true)
+
+  const handleCloseModal = () => setIsNewTransactionModal(false)
+
   return (
     <>
-      <Header />
+      <Header  onOpenNewTransactionModal={handleOpenModal}/>
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseModal}
+      />
       <GlobalStyle />
     </>
   );
